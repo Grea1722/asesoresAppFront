751e2dd960712e975220cedf2078a57ee7657f3e
Grea1722 <alexs17@live.com.mx>
2025-01-12 18:24:29 -0800

    Cantidades agregadas al state

--- src/context/pedidos/PedidoReducer.js
+++ src/context/pedidos/PedidoReducer.js
@@ -16,6 +16,15 @@ export default (state, action) => {
         ...state,
         productos: action.payload,
       };
+    case CANTIDAD_PRODUCTO:
+      return {
+        ...state,
+        productos: state.productos.map((producto) =>
+          producto.id === action.payload.id
+            ? (producto = action.payload)
+            : producto
+        ),
+      };
     default:
       return state;
   }
